ae0cac8358bc3e4b35cda65934581338fc8ca77a
Adisak Chaiyakul <adiaskchaiyakul@gmail.com>
2019-03-16 00:06:32 +0700

    line sdk

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+const line = require('@line/bot-sdk');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -18,9 +19,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-app.post('/webhook', (req, res) => {
+
+const config = {
+  channelAccessToken: 'yzrI9orBaIgmHhM4Av1FEVYPB2wcw6dBeo0ZdCy56Kmlidp6TPUdYLMOCScD+Gfs4Vu8+owfFOu4/mCXk+XRyYO84qvTx9wBgY5ZK9EoYxgY4DtjSSTODhgh+BnJO4egLzIONcvaiGcpgEP2q3iCvwdB04t89/1O/w1cDnyilFU=',
+  channelSecret: '1942d541bf0167673e65e5a094a88f13'
+};
+
+app.post('/webhook', line.middleware(config), (req, res) => {
   console.log('webhook', req.body);
-  res.send('success');
+  res.send('successeiei');
 });
 // app.use('/users', usersRouter);
 
