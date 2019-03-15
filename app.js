261364a9ffa726bf8334bf626fb091102d9d5a44
Adisak Chaiyakul <adiaskchaiyakul@gmail.com>
2019-03-16 00:02:17 +0700

    test webhook

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -18,9 +15,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+app.post('/webhook', (req, res) => {
+  console.log('webhook', req.body);
+  res.send('success');
+});
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
